c0789ac09b34dc1826b82156cf2ab6c032ebc18d
Wingmoon5150 <54482@student.eadania.dk>
2025-11-21 13:14:47 +0100

    added tcpclient thingy?

---
+++ MicroClient/MicroClient/TCPclient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroClient
+{
+    internal class TCPclient
+    {
+        TcpClient client = new TcpClient();
+
+        string hostname = "127.0.0.1";
+        int port = 2313;
+
+        public TCPclient()
+        {
+            client.Connect(hostname, port);
+        }
+    }
+}
